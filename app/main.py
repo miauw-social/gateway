2d27f62b4c5d134c7f1c462f1b0474b080fae9d2
Leo Breuer <contact@cheetahbyte.dev>
2023-07-14 20:08:44 +0200

    feat: add middleware for X-Process-Time header

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
+from fastapi import FastAPI, Depends
 from rabbit.rabbit import r, LOOP
 from routes.user import userRouter
+from routes.auth import authRouter
+from utils.guards import protected_route
 from uvicorn import Config, Server
+import time
 
 app = FastAPI(title="Gateway -  miauw")
 app.include_router(userRouter)
+app.include_router(authRouter)
 
+
+@app.middleware("http")
+async def add_process_time(req, call_next):
+    start = time.time()
+    resp = await call_next(req)
+    end = time.time() - start
+    resp.headers["X-Process-Time"] = str(end * 1000)
+    return resp
 
 @app.get("/")
 async def main(time: int = 0):
-    await asyncio.sleep(10)
     return "ok"
 
+
+@app.get("/test/", dependencies=[Depends(protected_route)])
+async def protected():
+    return "Test"
 
 @app.get("/health")
 async def health():
