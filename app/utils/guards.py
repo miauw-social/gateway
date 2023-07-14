f6e353ff94f82fefaccfe5657a5138a3a467b7cd
Leo Breuer <contact@cheetahbyte.dev>
2023-07-14 20:06:19 +0200

    feat: add protection for routes

---
+++ app/utils/guards.py
@@ -0,0 +1,11 @@
+from fastapi import Request, HTTPException
+from rabbit.rabbit import r
+
+async def protected_route(req: Request):
+    logged_in = await r.call("auth.session.exists", {"sid": req.cookies["session"]})
+    if not logged_in:
+        raise HTTPException(401, "not authorized")
+
+
+async def current_user():
+    pass
